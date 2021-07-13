f81104c3783056e5f9119f40d571c8b2065ca01f
KotaGit <Kota7arai@gmail.com>
2021-07-13 23:14:40 +0900

    待機状態、攻撃アニメージョン
    待機状態、攻撃アニメージョン

---
+++ Assets/Script/PlayerCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("攻撃");
+            animator.SetTrigger("shot");
+        }
+    }
+}

ce8c4b8d911a10488e35fa5b50f685df6ddf21c7
KotaGit <Kota7arai@gmail.com>
2021-07-22 15:46:16 +0900

    敵の消滅エフェクト実装
    敵の消滅エフェクト実装

--- Assets/Script/EnemyCtrl.cs
+++ Assets/Script/EnemyCtrl.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EnemyCtrl : MonoBehaviour
 {
     public int hp = 5;
+    public GameObject explosionPrefab;
 
     public void OnDamage()
     {
         hp -= 1;
         if(hp <= 0)
         {
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Script/ExplosionCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionCtrl : MonoBehaviour
+{
+
+    void Start()
+    {
+        var particleaSystem = GetComponent<ParticleSystem>();
+        Destroy(gameObject, particleaSystem.main.duration);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}

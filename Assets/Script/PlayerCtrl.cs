ced46a41bd37cbe7f56636947a82db802a6beb74
KotaGit <Kota7arai@gmail.com>
2021-07-25 15:57:57 +0900

    プレイヤーの移動実装
    プレイヤーの移動実装

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -4,22 +4,82 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    public Transform shotPoint;
-    public GameObject bulletPrefab;
+    #region//インスペクターで設定する
+    [Header("移動速度")] public float speed;
+    [Header("銃口")]public Transform shotPoint;
+    [Header("弾プレファブ")] public GameObject bulletPrefab;
     Animator animator;
+    [Header("右向きか")] bool isRight;
+    #endregion
+
+    #region//プライベート変数
+    private Rigidbody2D rd = null;
+    #endregion
 
     void Start()
     {
-        animator = GetComponent<Animator>();   
+        //コンポーネントのインスタンスを捕まえる
+        animator = GetComponent<Animator>();
+        rd = GetComponent<Rigidbody2D>();      //Rigidbody2D
+        isRight = true;
     }
 
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        Dierction(x);
+        Shot();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        //右入力で右に動く
+        if(horizontalKey > 0)
+        {
+            rd.velocity = new Vector2(speed, rd.velocity.y);
+            animator.SetBool("run", true);
+        }
+        //左入力で左に移動
+        else if(horizontalKey < 0)
+        {
+            rd.velocity = new Vector2(-speed, rd.velocity.y);
+            animator.SetBool("run", true);
+        }
+        //未入力で止まる
+        else
+        {
+            rd.velocity = Vector2.zero;
+            animator.SetBool("run", false);
+        }
+        
+    }
+   
+    void Dierction(float inputX)
+    {
+            //右向きで左入力なら180度回転
+        if(isRight && inputX < 0)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            isRight = false;
+        }
+
+            //左向きで右入力なら180度回転
+        if (!isRight && inputX > 0)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            isRight = true;
+        }
+    }
+
+    void Shot()
+    {
         if (Input.GetKeyDown("space"))      //弾を発射する処理
         {
-            
+
             animator.SetTrigger("shot");    //アニメーション「shot」
-            Instantiate(bulletPrefab, shotPoint.position,transform.rotation);
+            Instantiate(bulletPrefab, shotPoint.position, transform.rotation);
         }
     }
 }
